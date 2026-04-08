1e28ba7d5a25413b0735744ed9910dcbd6110e38
날다람쥐 <25esihoya@gmail.com>
2026-04-08 19:38:29 +0900

    test: exclude pure barrel entrypoints from coverage

--- src/edge.ts
+++ src/edge.ts
@@ -1,14 +1,8 @@
-import { createHonoCacheMiddleware } from './integrations/hono'
-import { PatternMatcher } from './invalidation/PatternMatcher'
-import { TagIndex } from './invalidation/TagIndex'
-import { MemoryLayer } from './layers/MemoryLayer'
-
 export { MemoryLayer } from './layers/MemoryLayer'
 export type { EvictionPolicy, MemoryLayerOptions, MemoryLayerSnapshotEntry } from './layers/MemoryLayer'
 export { PatternMatcher } from './invalidation/PatternMatcher'
 export { TagIndex } from './invalidation/TagIndex'
 export { createHonoCacheMiddleware } from './integrations/hono'
-void [MemoryLayer, PatternMatcher, TagIndex, createHonoCacheMiddleware]
 export type {
   CacheGetOptions,
   CacheLayer,
