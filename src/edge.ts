4769736a4f6042d0bdc5b7105c672224f5ddee29
날다람쥐 <25esihoya@gmail.com>
2026-04-08 19:46:26 +0900

    test: restore pure edge barrel

--- src/edge.ts
+++ src/edge.ts
@@ -1,19 +1,8 @@
-import { createHonoCacheMiddleware } from './integrations/hono'
-import { PatternMatcher } from './invalidation/PatternMatcher'
-import { TagIndex } from './invalidation/TagIndex'
-import { MemoryLayer } from './layers/MemoryLayer'
-
 export { MemoryLayer } from './layers/MemoryLayer'
 export type { EvictionPolicy, MemoryLayerOptions, MemoryLayerSnapshotEntry } from './layers/MemoryLayer'
 export { PatternMatcher } from './invalidation/PatternMatcher'
 export { TagIndex } from './invalidation/TagIndex'
 export { createHonoCacheMiddleware } from './integrations/hono'
-export const edgeExports = {
-  MemoryLayer,
-  PatternMatcher,
-  TagIndex,
-  createHonoCacheMiddleware
-} as const
 export type {
   CacheGetOptions,
   CacheLayer,
